427884b58d18550252184e1632bf7e04e7b75cab
qteng <tengqian0001@gmail.com>
2017-06-04 15:01:28 -0400

    add feature pagerank

--- code/feature_magic.py
+++ code/feature_magic.py
@@ -6,11 +6,12 @@ def _save(fname, data, protocol=4):
     # use protocol=4 to save files larger than 4GB
     with open(fname, "wb") as f:
         pickle.dump(data, f, protocol)
-
+# magic features from 0.158 kernel
 X_train = pd.read_pickle("../features/X_train.pkl")
 X_test = pd.read_pickle("../features/x_test.pkl")
 magic_xgb = pd.concat([X_train, X_test], axis = 0)
 
+# k_core
 dfTrain = pd.read_csv("../data/train.csv", encoding="ISO-8859-1")
 dfTest = pd.read_csv("../data/test.csv", encoding="ISO-8859-1")
 dfAll = pd.concat([dfTrain, dfTest], axis = 0, ignore_index=True)
@@ -18,12 +19,17 @@ for col in "question1", "question2":
     dfAll[col] = dfAll[col].str.lower()
 k_core = pd.read_csv("../features/question_kcores.csv")
 k_core.index = k_core.question
-
-
 magic_xgb['q1_k_core'] = dfAll["question1"].map(k_core['kcores']).values
 magic_xgb['q1_k_core'].fillna(1, inplace = True)
 magic_xgb['q2_k_core'] = dfAll["question2"].map(k_core['kcores']).values
 magic_xgb['q2_k_core'].fillna(1, inplace = True)
+
+# pagerank
+pagerank_train = pd.read_csv("../features/pagerank_train.csv")
+pagerank_test = pd.read_csv("../features/pagerank_test.csv")
+pagerank_all = pd.concat([pagerank_train, pagerank_test], axis = 0)
+magic_xgb['pagerank_q1'] = pagerank_all['q1_pr']
+magic_xgb['pagerank_q2'] = pagerank_all['q2_pr']
 
 X_train = magic_xgb.iloc[0:len(X_train)]
 x_test = magic_xgb.iloc[len(X_train):]
